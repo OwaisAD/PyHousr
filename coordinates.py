db3d83374aaf6f6df5049c0b429e99f98464bf2d
thomasfritzboger <thomasfritzboger@gmail.com>
2023-05-17 12:47:54 +0200

    kan hente data på flere huse, bug med vejnavne og koordinater på lejligheder, kvm pris i hele kr

--- coordinates.py
+++ coordinates.py
@@ -1,16 +1,15 @@
 from geopy.geocoders import Nominatim
-import pandas as pd
+
 
 def get_coordinates(address):
     geolocator = Nominatim(user_agent="my-app")
-
-    data = []
-    
     location = geolocator.geocode(address)
+    
     if location is not None:
-        data.append({'address': address, 'latitude': location.latitude, 'longitude': location.longitude})
+        x = location.latitude
+        y = location.longitude
     else:
-        data.append({'address': address, 'latitude': None, 'longitude': None})
+        x = None
+        y = None
 
-    df = pd.DataFrame(data)
-    return df.set_index('address').to_dict(orient='index')
+    return x, y
